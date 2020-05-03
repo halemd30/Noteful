eff94fa59097f75384f0979b5fa6b350cf12b18c
Mac Hale <machaledesign@gmail.com>
2020-05-03 11:28:53 -0600

    connecting to server

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 export default {
-  API_ENDPOINT: `https://localhost:9090/`,
-  //API_ENDPOINT: `https://boiling-bayou-66977.herokuapp.com/`,
+  //API_ENDPOINT: `https://localhost:9090/`,
+  API_ENDPOINT: `https://boiling-bayou-66977.herokuapp.com/`,
   API_KEY: process.env.REACT_APP_API_KEY
 }
